d2a3bd79bb9772282eadccec1663a3b7cae0bc7c
montjoile <montjoile@gmail.com>
2016-10-20 03:06:56 -0600

    adicion de amazon instance name en baseurl

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://ec2-35-160-55-25.us-west-2.compute.amazonaws.com/';
 
 /*
 |--------------------------------------------------------------------------
